6baf5e217fed4cf307151cf241bad9a51d22484c
aomom000 <6106021411097@fitm.kmutnb.ac.th>
2021-03-11 14:57:08 +0700

    update

--- db.php
+++ db.php
@@ -1,3 +1,8 @@
+<style>
+table, th, td {
+    border: 1px solid black;
+}
+</style>
 <?php
     define('hostname','localhost');
     define('user','root');
@@ -13,5 +18,23 @@
     }else{
         echo "connect sucsess";
     }
+    
+    $conn = "SELECT * FROM `book` where '1'";
+    $result = mysqli_query($mysql, $conn);
+
+    if ($result->num_rows > 0) {
+        echo "<table><tr><th>BookID</th><th>BookName</th><th>TypeID</th>
+        <th>StatusID</th><th>Publish</th><th>UnitPrice</th><th>UnitRent</th>
+        <th>DayAmount</th><th>Picture</th><th>BookDate</th></tr>";
+        while($row = $result->fetch_assoc()){
+        echo "<tr><td> " .$row["BookID"]."</td><td>" .$row["BookName"]
+                ."</td><td>" .$row["TypeID"]."</td><td>" .$row["StatusID"]
+                ."</td><td>" .$row["Publish"]."</td><td>" .$row["UnitPrice"]
+                ."</td><td>" .$row["UnitRent"]."</td><td>" .$row["DayAmount"]
+                ."</td><td>" .$row["Picture"]."</td><td>" .$row["BookDate"];
+        }
+    }
+
+    
     
 ?>
